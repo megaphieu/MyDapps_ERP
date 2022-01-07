dd3e6c66a7af2b0549585e2b6e77635b4fc065f2
megaphieu <megaphieu@gmail.com>
2022-01-07 18:17:28 +0700

    commit5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Web3 from "web3";
-// import { Button } from "react-bootstrap";
-import { useEffect, useState } from "react";
 import "./App.scss";
 import ProductList from "./components/ProductList";
-import { HELLO_ABI, HELLO_ADDRESS } from "./config";
-
-let web3;
-if (window.ethereum !== undefined) {
-    web3 = new Web3(window.ethereum);
-}
 
 function App() {
     return (
